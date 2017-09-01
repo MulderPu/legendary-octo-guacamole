c234223b06c7eb3a6d39b2cc320f8a04a2dfb4ec
MulderPu <mulderpu@gmail.com>
2017-09-01 20:58:43 +0800

    python integer

---
+++ pythonInteger.py
@@ -0,0 +1,9 @@
+user_input = input('Please enter an integer: ')
+
+n = int(user_input)
+nn = int(user_input+user_input)
+nnn = int(user_input+user_input+user_input)
+nnnn = int(user_input+user_input+user_input+user_input)
+
+print(n+nn+nnn+nnnn)
+
