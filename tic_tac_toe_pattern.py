90a377ca2bd11f9e15f16b3ad3accd489a5007e6
MulderPu <mulderpu@gmail.com>
2017-09-28 22:41:23 +0800

    print tic tac toe pattern with for loop

---
+++ tic_tac_toe_pattern.py
@@ -0,0 +1,8 @@
+for k in range(3):
+    for i in range(3):
+        print((' ' * (5)) + ('*') + (' '*(5)) + ('*') + (' '*(5)))
+    
+    if k != 2:
+        for j in range(1):
+            print('*'*18)
+
