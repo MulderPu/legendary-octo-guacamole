877eb6052569c2e5202fbcc4a502a074a6119a54
MulderPu <mulderpu@gmail.com>
2017-10-02 20:43:19 +0800

    incomplete

--- codewars/string_incrementer.py
+++ codewars/string_incrementer.py
@@ -2,17 +2,11 @@ def increment_string(string):
     last = string[-1:]
     try:
         if int(last):
-            print(last)
             new_number = int(last) + int(1)
             string = string[:-1]
             string += str(new_number)
     except ValueError:
-        print('err',last)
         string += str(1)
     return string
-string = 'foobar00'
-
-s="0"
-if int(s):
-    print('this is a number')
+string = 'foobar1'
 print(increment_string(string))
